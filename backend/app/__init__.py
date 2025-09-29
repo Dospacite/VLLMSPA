3241396948b931c1f32e08ac86e6cdbe2b6d7961
Ege Güray Ertan <54844588+Dospacite@users.noreply.github.com>
2025-09-29 11:37:44 +0300

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from flask_migrate import Migrate
 from .config import Config
 
 db = SQLAlchemy()
